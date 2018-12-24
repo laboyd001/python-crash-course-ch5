64fa1e3a6d34c7662d8d66a9b986f3554dcc2c87
Lesley Boyd <lesley.a.boyd1@gmail.com>
2018-12-24 12:01:21 -0600

    conditional testing exercises

---
+++ conditional_tests.py
@@ -0,0 +1,25 @@
+#write a series of conditional tests.  Print a statement describing each test and your prediction for the results of each test.
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'?  I predict False.")
+print(car == 'audi')
+
+age = 20
+print("\nShould a 20 year old still be on their parent's insurance?")
+print(age < 25)
+
+name = 'molly'
+print("\nIs your name Sally?")
+print(name.lower() == 'sally')
+
+number = 10
+print("\nIs my number between 1 and 100?")
+print( number >= 1 and number <=100)
+
+favorite_colors = ['salmon', 'seafoam', 'gray']
+print("\nIs brown one of my favorite colors?")
+print('brown' in favorite_colors)
+
---
+++ toppings.py
@@ -0,0 +1,6 @@
+#checking for inequality
+
+requested_toppings = 'mushrooms'
+
+if requested_toppings != 'anchovies':
+    print("Hold the anchovies!")
